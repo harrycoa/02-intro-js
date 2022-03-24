dc87fb9cccdd03ee9b15f88a849aa95600ebb884
Harry Coa <rodol_2000@hotmail.com>
2022-03-24 17:44:51 -0500

    fix

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import  heroes,  { owners  } from "./data/heroes";
+
+
+const getHeroeById = (id) => {
+    return heroes.find( (heroe) => heroe.id === id);
+}
+
+
+const getHeroeByOwner = (owner) => {
+    return heroes.filter( (heroedc) => heroedc.owner === owner );
+}
+
+
+console.log( getHeroeById (3));
+console.log( getHeroeByOwner ('DC'));
+
+
+console.log(owners);
